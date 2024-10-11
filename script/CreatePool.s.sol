f22e53dda5b72fd288f133b59ae2e8254aa1dae2
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2024-10-11 08:08:37 +0700

    rename script file

---
+++ script/CreatePool.s.sol
@@ -0,0 +1,27 @@
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.13;
+
+// import {Script, console} from "forge-std/Script.sol";
+// import {FinkiCoin} from "../src/FinkiCoin.sol";
+
+// interface IUniswapV2Factory {
+//     function createPair(address tokenA, address tokenB) external returns (address pair);
+// }
+
+// contract FinkiCoinScript is Script {
+
+//     FinkiCoin public finkiCoin;
+
+//     function setUp() public {}
+
+//     function run() public {
+//         vm.startBroadcast();
+
+//         IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f).createPair(
+//             , 
+//             0x779877A7B0D9E8603169DdbD7836e478b4624789
+//         );        
+
+//         vm.stopBroadcast();
+//     }
+// }
